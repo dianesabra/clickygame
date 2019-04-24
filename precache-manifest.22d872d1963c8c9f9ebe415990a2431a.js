53049bafe8a01fe7ca0f1d6ac5d4ff33a649eb48
dianesabra <dwsabra@gmail.com>
2019-04-23 20:42:08 -0500

    Updates

---
+++ precache-manifest.22d872d1963c8c9f9ebe415990a2431a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "2c001024767a71e121c3",
+    "url": "/clickygame/static/js/main.17e6c7a5.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2c001024767a71e121c3",
+    "url": "/clickygame/static/css/main.d2743686.chunk.css"
+  },
+  {
+    "revision": "10553637968a27f4a2f3e84a419b6946",
+    "url": "/clickygame/index.html"
+  }
+];
